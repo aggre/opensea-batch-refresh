d0c36070c32a9c5a31368682fd3097a805a0e4aa
aggre <hiroyuki.aggre@gmail.com>
2022-09-01 11:03:24 +0900

    fix

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
+import PQueue from 'p-queue'
 import puppeteer from 'puppeteer'
 
-const MAX_TOKEN_ID = 3
+const queue = new PQueue({ concurrency: 1 })
+
+const MAX_TOKEN_ID = 80
 const BASE =
   'https://opensea.io/assets/matic/0x89904de861cded2567695271a511b3556659ffa2'
 const SELECTOR = '#main button:first-child'
@@ -12,20 +15,22 @@ const SELECTOR = '#main button:first-child'
   const page = await browser.newPage()
   console.log('browser prepared')
 
-  const promises = new Array(MAX_TOKEN_ID).fill(null).map(async (_, i) => {
-    const id = i + 1
-    await page.goto(`${BASE}/${id}`)
-    console.log(id, 'page loaded')
-    await page.waitForSelector(SELECTOR)
-    console.log(id, 'refresh button found')
-    page.click(SELECTOR), console.log(id, 'clicked the refresh button')
-    await page.waitForSelector('[data-testid=toasts]')
-    console.log(id, 'finished')
-  })
+  const promises = new Array(MAX_TOKEN_ID)
+    .fill(null)
+    .map((_, i) => async () => {
+      const id = i + 1
+      await page.goto(`${BASE}/${id}`)
+      console.log(id, 'page loaded')
+      await page.waitForSelector(SELECTOR)
+      console.log(id, 'refresh button found')
+      page.click(SELECTOR), console.log(id, 'clicked the refresh button')
+      await page.waitForSelector('[data-testid=toasts]')
+      console.log(id, 'finished')
+    })
 
-  const results = await Promise.allSettled(promises)
+  const results = await queue.addAll(promises)
 
-  results.forEach((result, i) => console.log(i + 1, result.status))
+  results.forEach((result, i) => console.log(i + 1, result))
 
   browser.close()
 })()
