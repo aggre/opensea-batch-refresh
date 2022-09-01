9d130f6d9d1804ed55f52150e4c2cfb3f6257b62
aggre <hiroyuki.aggre@gmail.com>
2022-09-01 10:57:40 +0900

    impl

--- index.mjs
+++ index.mjs
@@ -1,29 +1,31 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer'
 
-const MAX_TOKEN_ID = 3;
+const MAX_TOKEN_ID = 3
 const BASE =
-  "https://opensea.io/assets/matic/0x89904de861cded2567695271a511b3556659ffa2";
+  'https://opensea.io/assets/matic/0x89904de861cded2567695271a511b3556659ffa2'
+const SELECTOR = '#main button:first-child'
 
-(async () => {
+;(async () => {
   const browser = await puppeteer.launch({
     headless: false,
-  });
-  const page = await browser.newPage();
-  console.log("browser prepared");
+  })
+  const page = await browser.newPage()
+  console.log('browser prepared')
 
-  Promise.all(
-    new Array(MAX_TOKEN_ID).fill(null).map(async (_, i) => {
-      const id = i + 1;
-      await page.goto(`${BASE}/${id}`);
-      console.log(id, "page loaded");
-      await page.waitForSelector("#main button:first-child");
-      console.log(id, "refresh button found");
-      await page.evaluate(() => {
-        document.querySelector("#main button:first-child").click();
-        console.log(id, "clicked the refresh button");
-      });
-      await page.waitForSelector("[data-testid=toasts]");
-      console.log(id, "finished");
-    })
-  );
-})();
+  const promises = new Array(MAX_TOKEN_ID).fill(null).map(async (_, i) => {
+    const id = i + 1
+    await page.goto(`${BASE}/${id}`)
+    console.log(id, 'page loaded')
+    await page.waitForSelector(SELECTOR)
+    console.log(id, 'refresh button found')
+    page.click(SELECTOR), console.log(id, 'clicked the refresh button')
+    await page.waitForSelector('[data-testid=toasts]')
+    console.log(id, 'finished')
+  })
+
+  const results = await Promise.allSettled(promises)
+
+  results.forEach((result, i) => console.log(i + 1, result.status))
+
+  browser.close()
+})()
