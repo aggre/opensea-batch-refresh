4a32ce7affb044c61b5c558c6c2de15cd02522c8
aggre <hiroyuki.aggre@gmail.com>
2023-10-12 11:44:02 +0900

    it worked

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const queue = new PQueue({ concurrency: 1 })
 const MAX_TOKEN_ID = 120
 const BASE =
   'https://opensea.io/assets/matic/0x89904de861cded2567695271a511b3556659ffa2'
-const MORE_SELECTOR = 'main button[aria-label="More"]'
+const MORE_SELECTOR = 'button[aria-label="More"]'
 const REFRESH_SELECTOR = '.tippy-content button'
 
 ;(async () => {
@@ -25,15 +25,19 @@ const REFRESH_SELECTOR = '.tippy-content button'
         console.log(id, 'page loaded')
         await page.waitForSelector(MORE_SELECTOR)
         console.log(id, 'more button found')
+        await page.hover(MORE_SELECTOR)
+        console.log(id, 'hover on more button')
         page.click(MORE_SELECTOR), console.log(id, 'clicked the more button')
         await page.waitForSelector(REFRESH_SELECTOR)
         console.log(id, 'refresh button found')
+        await page.hover(REFRESH_SELECTOR)
+        console.log(id, 'hover on refresh button')
         page.click(REFRESH_SELECTOR), console.log(id, 'clicked the refresh button')
         await page.waitForSelector('[data-testid=toasts]')
         console.log(id, 'finished')
         return [id, 'success']
       } catch (error) {
-        return [id, 'failed', error]
+        return ['failed', error]
       }
     })
 
